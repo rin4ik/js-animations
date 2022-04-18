7ff58f425c28d5efe87db4c62c068c32bd44b921
rin4ik <m.mirakhmad@gmail.com>
2022-04-18 10:15:01 +0500

    validations

--- form/app.js
+++ form/app.js
@@ -17,7 +17,58 @@ containers.forEach(container => {
             )
             tl.to(line, {attr: {d: start}, ease: 'elastic.out(3, 0.5)'}, '<50%');
             // Placeholder shift
-            tl.to(placeholder, {top: -15, left: 0, scale: 0.7, duration: 0.5, ease: "Power2.easeOut"})
+            tl.to(placeholder, {top: -15, left: 0, scale: 0.7, duration: 0.5, ease: "Power2.easeOut"}, '<15%')
         }
     })
 })
+form.addEventListener('click', () => {
+    containers.forEach(container => {
+        const input = container.querySelector('.input') 
+        const line = container.querySelector('.elastic-line')
+        const placeholder = container.querySelector('.placeholder')
+        if(document.activeElement !== input) {
+            if(!input.value) {
+                gsap.to(placeholder, {top: 0, left: 0, scale: 1, duration: 0.5, ease: "Power2.easeOut"})
+            }
+        }
+        input.addEventListener('input', (e) => {
+            if(e.target.type === 'text') {
+                let inputText = e.target.value;
+                if(inputText.length > 2) {
+                    colorize('#6391e8', line, placeholder)
+                } else {
+                    colorize('#FE8C99', line, placeholder)
+                }
+            }
+            if(e.target.type === 'email') {
+                let valid = validateEmail(e.target.value);
+                if(valid) {
+                    colorize('#6391e8', line, placeholder)
+                } else {
+                    colorize('#FE8C99', line, placeholder)
+                }
+            }
+            if(e.target.type === 'tel') {
+                let valid = validatePhone(e.target.value);
+                if(valid) {
+                    colorize('#6391e8', line, placeholder)
+                } else {
+                    colorize('#FE8C99', line, placeholder)
+                }
+            }
+        })
+    })
+})
+
+function validateEmail(email) {
+    let re = /\S+@\S+\.\S+/
+    return re.test(email)
+}
+function validatePhone(phone) {
+    let re = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+    return re.test(phone)
+}
+function colorize(color, line, placeholder) {
+    gsap.to(line, {stroke: color, duration: 0.75})
+    gsap.to(placeholder, {color: color, duration: 0.75})
+}
