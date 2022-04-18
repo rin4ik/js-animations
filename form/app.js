8fe14b00b9f806fa4e7aa023bd57a9599d913734
rin4ik <m.mirakhmad@gmail.com>
2022-04-18 15:01:13 +0500

    style fixes

--- form/app.js
+++ form/app.js
@@ -6,7 +6,7 @@ const start = "M0 0.999512C0 0.999512 60.5 0.999512 150 0.999512C239.5 0.999512 
 const end = "M1 0.999512C1 0.999512 61.5 7.5 151 7.5C240.5 7.5 301 0.999512 301 0.999512"
 
 containers.forEach(container => {
-    const input = container.querySelector('.input') 
+    const input = container.querySelector('.input')
     const line = container.querySelector('.elastic-line')
     const placeholder = container.querySelector('.placeholder')
     input.addEventListener('focus', () => {
@@ -72,3 +72,22 @@ function colorize(color, line, placeholder) {
     gsap.to(line, {stroke: color, duration: 0.75})
     gsap.to(placeholder, {color: color, duration: 0.75})
 }
+const checkbox = document.querySelector('.checkbox')
+const tl2 = gsap.timeline({defaults: { duration: 0.5, ease: "Power2.easeOut"}})
+const tickMarkPath = document.querySelector('.tick-mark path')
+const pathLength = tickMarkPath.getTotalLength()
+gsap.set(tickMarkPath, {strokeDashoffset: pathLength, strokeDasharray: pathLength })
+checkbox.addEventListener('click', () => {
+    if(checkbox.checked) {
+        tl2.to('.checkbox-fill', {top: "0%"})
+        tl2.fromTo(tickMarkPath, {strokeDashoffset: pathLength}, {strokeDashoffset: 0}, '<50%')
+        tl2.to('.checkbox-label', {color: '#6391e8'}, '<')
+    } else {
+        tl2.to('.checkbox-fill', {top: '100%'})
+        tl2.fromTo(tickMarkPath, {strokeDashoffset: 0}, {strokeDashoffset: pathLength}, '<50%')
+        tl2.to('.checkbox-label', {color: '#c5c5c5'}, '<')
+    }
+})
+gsap.set('#eye', {transfromOrigin: 'center'})
+gsap.fromTo('#eye', {scaleY: 1}, {scaleY: 0.3, repeat: -1, yoyo: true, repeatDelay: 0.5,  ease: "Power2.easeOut"})
+gsap.fromTo('#eyebrow', {y:0}, {y:-1, repeat: -1, yoyo: true, repeatDelay: 0.5, ease: 'Power2.easeOut'})
