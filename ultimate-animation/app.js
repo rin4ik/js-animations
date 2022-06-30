25fdee210981ae2abc81eb538b9d556c8a18c727
Mirakhmad <m.mirakhmad@gmail.com>
2022-06-30 17:57:41 +0500

    animating wip

--- ultimate-animation/app.js
+++ ultimate-animation/app.js
@@ -32,12 +32,47 @@ const tlHRemove = gsap.timeline({
 tlHRemove.to('.highlight', 
     {color: 'rgba(255,255,255, 0.4', stagger: 1}
 )
-// const tlIntro = gsap.timeline({
-//     scrollTrigger: {
-//         trigger: '.first-page',
-//         start: "0%",
-//         end: "100%",
-//         scrub: true,
-//     }
-// })
-// tlIntro.fromTo('nav', {opacity: 1}, {opacity: 0, duration: 1})
+const tlSplit = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.third-page',
+        start: '-25%',
+        end: '30%',
+        scrub: true,
+        markers: true
+    },
+})
+tlSplit.fromTo('.large-phone', {x: '40%'}, {x: '20%'})
+tlSplit.fromTo('.small-phone', {x: '-40%'}, {x: '-20%'}, '<')
+tlSplit.fromTo('.product-text-left', {x: 50, opacity: 0}, {opacity: 1, x: 0}, '<')
+tlSplit.fromTo('.product-text-right', {x: -50, opacity: 0}, {opacity: 1, x: 0}, '<')
+
+const tlSplitPin = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.third-page',
+        pin: true,
+        pinSpacing: false,
+        start: "0%",
+        end: '100%'
+    }
+})
+
+const swatches = document.querySelectorAll('.swatches img')
+const gallery = document.querySelectorAll('.phone-gallery')
+const slides = document.querySelectorAll('.phone-gallery-container')
+
+let currentSwatch = 'blue'
+let topIndex = 2;
+
+swatches.forEach((swatch, index) => {
+    const coord = slides[index].getBoundingClientRect().left
+    swatch.addEventListener('click', (e) => {
+        let swatchName = e.target.getAttribute('swatch')
+        let closeUp = document.querySelector('.' + swatchName)
+        if(currentSwatch === swatchName) return;
+        gsap.set(closeUp, {zIndex: topIndex})
+        gsap.fromTo(closeUp, {opacity: 0}, {opacity: 1, duration: 1})
+        gsap.to(gallery, {x: -coord, duration: 1, ease: 'back.out(1)'})
+        topIndex++;
+        currentSwatch = swatchName
+    })
+});
